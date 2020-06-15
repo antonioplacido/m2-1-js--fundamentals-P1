ce4200fb6e3ae709f53f27ba2d90f1e0d9f98d20
Antonio Placido <tonyplacido@live.com>
2020-06-15 17:58:30 -0400

    Question 9

--- workshop/exercise-9.js
+++ workshop/exercise-9.js
@@ -24,3 +24,13 @@
 // FizzBuzz
 // 16
 // ...
+
+for(x=1;x<=100;x++)
+    if(x % 3 ===0 && x % 5 !==0)
+    {console.log(`Fizz`)}
+    else if (x % 5 && x % 3 !==0)
+    {console.log(`Buzz`)}
+    else if (x % 3 ===0 && x % 5 ===0 )
+    {console.log(`FizzBuzz`)}
+    else{console.log(`${x}`)}
+
